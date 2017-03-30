cb7b77039ee1742e20014f7af9718146eb750a05
Kamil <stacho688@gmail.com>
2017-03-30 13:47:59 +0200

    Added main class

--- src/kamil_stachowicz/mainClass.java
+++ src/kamil_stachowicz/mainClass.java
@@ -4,4 +4,8 @@ package kamil_stachowicz;
  * Created by staho on 30.03.2017.
  */
 public class mainClass {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Witaj padole łez :(");
+    }
 }
