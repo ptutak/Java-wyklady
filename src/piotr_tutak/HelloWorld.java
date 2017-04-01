0fc0f35f44c1d6c726a62903d08ae8cb112e55d2
ptutak <p.tutak@outlook.com>
2017-04-01 09:44:38 +0200

    InOutErr, prezentacja

--- src/piotr_tutak/HelloWorld.java
+++ src/piotr_tutak/HelloWorld.java
@@ -1,10 +1,7 @@
 package piotr_tutak;
 
-public class TestClass {
-
+public class HelloWorld {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
-
 }
